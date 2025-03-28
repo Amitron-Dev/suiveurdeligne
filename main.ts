1e893225f1ee9fc194c7ca3ea80152508864990c
Amitron-Dev <martingrellier79@gmail.com>
2025-03-28 13:14:03 +0100

    Final

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    activate = 1
+})
+let activate = 0
+let moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(2)
+moveMotorZIP.showColor(Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.White))
 basic.forever(function () {
-	
+    if (activate == 1) {
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, 26)
+        if (Kitronik_Move_Motor.readSensor(Kitronik_Move_Motor.LfSensor.Left) > 350) {
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorRight)
+            basic.pause(20)
+        } else {
+            Kitronik_Move_Motor.motorOff(Kitronik_Move_Motor.Motors.MotorLeft)
+            basic.pause(20)
+        }
+    }
 })
